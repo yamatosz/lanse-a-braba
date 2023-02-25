ca40e172de8eba2be0dba6c694669217f92cddf1
Lucas José <90628627+yamatosz@users.noreply.github.com>
2023-02-25 08:50:03 -0300

    date changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Contagem from "./countdown.js";
 
-const lanse_a_braba_cont = new Contagem("13 November 2022 23:00:00 GMT-0300");
+const lanse_a_braba_cont = new Contagem("13 November 2023 18:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function exibeTime() {
